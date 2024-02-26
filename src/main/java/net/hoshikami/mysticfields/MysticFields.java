28488e69874010417318c5875134dea4540c2d03
JulianSureda <hoshikamiTV@protonmail.com>
2024-02-27 00:25:47 +0100

    Moonshard added + Mod Creative tab

--- src/main/java/net/hoshikami/mysticfields/MysticFields.java
+++ src/main/java/net/hoshikami/mysticfields/MysticFields.java
@@ -1,6 +1,10 @@
 package net.hoshikami.mysticfields;
 
 import com.mojang.logging.LogUtils;
+import net.hoshikami.mysticfields.item.ModCreativeModeTabs;
+import net.hoshikami.mysticfields.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -27,6 +31,9 @@ public class MysticFields
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModItems.register(modEventBus);
+        ModCreativeModeTabs.register(modEventBus);
+
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
@@ -42,6 +49,9 @@ public class MysticFields
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.CRYSTALLIZED_MOONLIGHT);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/hoshikami/mysticfields/item/ModCreativeModeTabs.java
@@ -0,0 +1,27 @@
+package net.hoshikami.mysticfields.item;
+
+import net.hoshikami.mysticfields.MysticFields;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MysticFields.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> MYSTIC_FIELDS_TAB = CREATIVE_MODE_TABS.register("mystic_fields_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.CRYSTALLIZED_MOONLIGHT.get()))
+                    .title(Component.translatable("creativetab.mystic_fields_tab"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.CRYSTALLIZED_MOONLIGHT.get());
+                    })
+                    .build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
